9f4e2b27ac32393b3d99d258f15ff1488c87bfcb
tolu2507 <tolusnehhil@gmail.com>
2023-01-12 17:14:53 +0100

    new commit

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-
+  public Details: string[] = [
+    '07038968337, 09025549043',
+    'Baale Akanji Layout, Jobele, Oyo, Oyo State.',
+  ];
+  public User = {
+    surname: 'BAMISILE',
+    fullname: 'TOLULOPE ADETAYO',
+    stack: 'FULLSTACK/MOBILEAPP DEVELOPER',
+    email: 'Tolulopebamisile@gmail.com',
+  };
 }
